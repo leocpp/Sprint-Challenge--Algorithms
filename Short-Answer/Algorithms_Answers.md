16c7f6248314004f43b5cf084965acb0c33c7611
Leo Lam <leo.cpp@gmail.com>
2019-12-13 10:44:08 -0800

    SC

--- Short-Answer/Algorithms_Answers.md
+++ Short-Answer/Algorithms_Answers.md
@@ -3,13 +3,15 @@
 ## Exercise I
 
 a)
-
+O(n)
 
 b)
-
+O(n**2)
 
 c)
+O(n)
 
 ## Exercise II
+Start from the middle. Move half way between the top if it does not break.  Move down half way between the ground if it breaks.  Repeat.
 
-
+O(Log(n))
--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -3,8 +3,21 @@ Your function should take in a single parameter (a string `word`)
 Your function should return a count of how many occurences of ***"th"*** occur within `word`. Case matters.
 Your function must utilize recursion. It cannot contain any loops.
 '''
+
 def count_th(word):
-    
+
+    count = 0
     # TBC
-    
-    pass
+    pos = word.find('th')
+
+    if pos == -1:
+        return 0
+
+    return 1  + count_th(word[pos+2:])
+
+word = 'thththt'
+
+print(count_th(word))
+word = 'aathtaahtht'
+
+print(count_th(word))
